9dd31f74bda7d5f6a10fdfe0258afc45b263b057
Mostafa ELareef <121424728+mostafael3ref@users.noreply.github.com>
2025-11-12 18:10:11 +0300

    Update1 desktop.py

--- health_safety/config/desktop.py
+++ health_safety/config/desktop.py
@@ -1,10 +1,13 @@
+# -*- coding: utf-8 -*-
 from frappe import _
 
 def get_data():
-    return [
-        {
-            "module_name": "Health and Safety",
-            "type": "module",
-            "label": _("Health and Safety")
-        }
-    ]
+    return [{
+        "module_name": "Health & Safety",
+        "category": "Modules",
+        "label": _("Health & Safety"),
+        "color": "orange",
+        "icon": "octicon octicon-shield",
+        "type": "module",
+        "hidden": 0,
+    }]
