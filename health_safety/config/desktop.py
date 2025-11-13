72f44cd41294c1ee407e85acc704b1094a6f053e
Mostafa ELareef <121424728+mostafael3ref@users.noreply.github.com>
2025-11-13 09:32:47 +0300

    Update4 desktop.py

--- health_safety/config/desktop.py
+++ health_safety/config/desktop.py
@@ -1,12 +1,10 @@
-# apps/health_safety/health_safety/config/desktop.py
-# -*- coding: utf-8 -*-
 from frappe import _
 
 def get_data():
     return [{
-        "module_name": "Health and Safety",   # <-- مهم: يطابق modules.txt
+        "module_name": "health_safety",   # لازم يطابق modules.txt
         "category": "Modules",
-        "label": _("Health & Safety"),        # العرض للمستخدم، عادي فيه &
+        "label": _("Health & Safety"),    # ليبل العرض للمستخدم.. خليه زي ما تحب
         "color": "orange",
         "icon": "octicon octicon-shield",
         "type": "module",
