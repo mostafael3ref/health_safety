27cb3923c1f21bff7a9d947bd13c228f3b2a2913
Mostafa ELareef <121424728+mostafael3ref@users.noreply.github.com>
2025-11-16 23:24:43 +0300

    Update equipment.py

--- health_safety/health_safety/doctype/equipment/equipment.py
+++ health_safety/health_safety/doctype/equipment/equipment.py
@@ -1,1 +1,15 @@
+# health_safety/health_safety/doctype/equipment/equipment.py
 
+import frappe
+from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+
+class Equipment(Document):
+    def before_insert(self):
+        # Generate unique Stomach Number if empty
+        if not self.stomach_number:
+            self.stomach_number = make_autoname("EQU-.YYYY.-.####")
+
+        # Set document name as stomach_number
+        if not self.name:
+            self.name = self.stomach_number
