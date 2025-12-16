7fbd4eea9729263c30c6bfdd2476f8a48d187e71
Mostafa ELareef <121424728+mostafael3ref@users.noreply.github.com>
2025-12-17 02:31:53 +0300

    Create ladders_checklist.py

---
+++ health_safety/health_safety/doctype/ladders_checklist/ladders_checklist.py
@@ -0,0 +1,14 @@
+import frappe
+from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+
+
+class LaddersChecklist(Document):
+    def before_insert(self):
+        # Generate unique ID if empty (LC-YYYY-0001)
+        if not self.checklist_id:
+            self.checklist_id = make_autoname("LC-.YYYY.-.####")
+
+        # Use checklist_id as the document name
+        if not self.name:
+            self.name = self.checklist_id
