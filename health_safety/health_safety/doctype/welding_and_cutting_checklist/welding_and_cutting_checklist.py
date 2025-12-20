3b84c7730c5cc595070caa678a3ff446754e8b28
Mostafa ELareef <121424728+mostafael3ref@users.noreply.github.com>
2025-12-20 05:51:46 +0300

    Update1 welding_and_cutting_checklist.py

--- health_safety/health_safety/doctype/welding_and_cutting_checklist/welding_and_cutting_checklist.py
+++ health_safety/health_safety/doctype/welding_and_cutting_checklist/welding_and_cutting_checklist.py
@@ -5,10 +5,7 @@ from frappe.model.naming import make_autoname
 
 class WeldingAndCuttingChecklist(Document):
     def before_insert(self):
-        # Auto-generate Checklist ID: WCC-YYYY-0001
         if not self.checklist_id:
             self.checklist_id = make_autoname("WCC-.YYYY.-.####")
 
-        # Use checklist_id as document name
-        if not self.name:
-            self.name = self.checklist_id
+        self.name = self.checklist_id
