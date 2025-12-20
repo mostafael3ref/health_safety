c80c0a063f38b7b8b6e20fa9b6427583e091d366
Mostafa ELareef <121424728+mostafael3ref@users.noreply.github.com>
2025-12-20 05:28:48 +0300

    Update13 install.py

--- health_safety/install.py
+++ health_safety/install.py
@@ -567,7 +567,3 @@ def after_install():
     create_machinery_equipment_checklist_print_format()
 
     create_water_letter_head()
-
-def after_migrate():
-    # نخلي نفس خطوات after_install تتنفذ بعد أي migrate
-    after_install()
