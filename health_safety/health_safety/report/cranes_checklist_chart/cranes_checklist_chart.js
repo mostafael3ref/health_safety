5c674015dee26533001b0dba080a5166debc3021
Mostafa ELareef <121424728+mostafael3ref@users.noreply.github.com>
2026-01-07 16:47:50 +0300

    Update1 cranes_checklist_chart.js

--- health_safety/health_safety/report/cranes_checklist_chart/cranes_checklist_chart.js
+++ health_safety/health_safety/report/cranes_checklist_chart/cranes_checklist_chart.js
@@ -3,12 +3,14 @@ frappe.query_reports["Cranes Checklist Chart"] = {
     {
       fieldname: "from_date",
       label: "From Date",
-      fieldtype: "Date"
+      fieldtype: "Date",
+      default: frappe.datetime.month_start()
     },
     {
       fieldname: "to_date",
       label: "To Date",
-      fieldtype: "Date"
+      fieldtype: "Date",
+      default: frappe.datetime.get_today()
     }
   ]
 };
