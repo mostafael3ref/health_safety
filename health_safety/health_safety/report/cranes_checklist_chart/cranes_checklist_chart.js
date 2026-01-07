e19f3b3d9fbb957e0c4eb30f1d781574adad0f7b
Mostafa ELareef <121424728+mostafael3ref@users.noreply.github.com>
2026-01-07 16:44:10 +0300

    Create cranes_checklist_chart.js

---
+++ health_safety/health_safety/report/cranes_checklist_chart/cranes_checklist_chart.js
@@ -0,0 +1,14 @@
+frappe.query_reports["Cranes Checklist Chart"] = {
+  filters: [
+    {
+      fieldname: "from_date",
+      label: "From Date",
+      fieldtype: "Date"
+    },
+    {
+      fieldname: "to_date",
+      label: "To Date",
+      fieldtype: "Date"
+    }
+  ]
+};
